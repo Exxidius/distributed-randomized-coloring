0cebbc5466d1aa3c57538055e39ad0f3ccf25908
Exxidius <matti.grilz92@gmail.com>
2024-01-13 13:15:01 +0100

    finished with doc

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Distributed-Randomized-Coloring
 Implementation of a Distributed Randomized Coloring algorithm
+
+Usage:
+```
+main.py [-h] [-g] [-c] [-v VERTICES] [-e EDGES]
+
+Randomized Distributed Algorithm
+
+options:
+  -h, --help            show this help message and exit
+  -g, --pgraph          Print graph
+  -c, --pcoloring       Print coloring
+  -v VERTICES, --vertices VERTICES
+                        Number of vertices
+  -e EDGES, --edges EDGES
+                        Number of edges
+```
+
+Examples:
+  - 10 vertices, 10 edges: ``python3 main.py -gc -v 10 -e 10``
+  - 400 vertices, 10000 edges: ``python3 main.py -gc -v 10 -e 10``
+  - 10 vertices, 10 edges, no graph output, no coloring output ``python3 main.py -v 10 -e 10``
+  - 10 vertices, 10 edges, only coloring output  ``python3 main.py -c -v 10 -e 10``
+  - 10 vertices, 10 edges, only graph output  ``python3 main.py -g -v 10 -e 10``
+
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import sys
+import argparse
 import random
 
 def randomizedColoring(graph, degree):
     uncolored_vertices = list(graph.keys())
-    perm_colors = dict()
+    permanent_colors = dict() 
     color_nums = degree + 1 
+    colors_used = set() 
 
-    n = 0
+    n_iterations = 0
     while(len(uncolored_vertices) > 0):
-        cand_colors = dict()
+        candidate_colors = dict()
         for v in uncolored_vertices:
-            possible_colors = possibleColors(graph, perm_colors, v, color_nums)
-            cand_colors[v] = random.sample(possible_colors, 1)
+            possible_colors = possibleColors(graph, permanent_colors,
+                                             v, color_nums)
+            candidate_colors[v] = random.sample(possible_colors, 1)
 
-        for v in cand_colors:
-            desired_color = cand_colors[v]
-            already_taken = False
-            for nb in graph[v]:
-                if(nb in perm_colors and desired_color == perm_colors[nb]):
-                    already_taken = True
-                    break
-            if(not already_taken):
-                perm_colors[v] = desired_color
+        for v in candidate_colors:
+            if(permanentlyColor(graph, candidate_colors, v)):
+                permanent_colors[v] = candidate_colors[v]
                 uncolored_vertices.remove(v)
-        n += 1
-    print(n)
-    return perm_colors
+                colors_used.add(candidate_colors[v][0])
+        n_iterations += 1
+
+    return permanent_colors, n_iterations, len(colors_used)
+
+def permanentlyColor(graph, candidate_colors, vertex):
+    desired_color = candidate_colors[vertex]
+    for nb in graph[vertex]:
+        if(nb in candidate_colors and desired_color == candidate_colors[nb]):
+            return False
+    return True
 
 def possibleColors(graph, colored_vertices, vertex, number_colors):
     colors = list(range(1, number_colors + 1))
@@ -65,37 +70,46 @@ def testAlgorithm(graph, coloring):
     return True
 
 def printUsage():
-    print("Usage: python3 main.py num_vertices num_edges")
+    print("Usage: python3 main.py print_result num_vertices num_edges")
+    print("     print_result: bool, print the final coloring and graph")
     print("     num_vertices: int, create a graph with num_vertices vertices")
     print("     num_edges:  int, create a graph wiht num_edges edges")
 
+def printStatistics(degree, n_iterations, num_used):
+    colors_num = degree + 1
+    print(f'Degree of graph is: {degree}')
+    print(f'Number of colors possible: {colors_num}')
+    print(f'Number of colors used: {num_used}')
+    print(f'Computation took {n_iterations} iterations')
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description="Randomized Distributed Algorithm")
+    parser.add_argument('-g', '--pgraph', default=False, action='store_true',
+                        help="Print graph")
+    parser.add_argument('-c', '--pcoloring', default=False, action='store_true',
+                        help="Print coloring")
+    parser.add_argument('-v', '--vertices', default=300, type=int,
+                        help="Number of vertices")
+    parser.add_argument('-e', '--edges', default=300, type=int,
+                        help="Number of edges")
+
+    return parser.parse_args()
+
 def main():
-    num_vertices = 300
-    num_edges = 300
-    argc = len(sys.argv)
-
-    if argc == 2 or argc > 3:
-        printUsage()
-        exit(-1)
-
-    if argc == 3:
-        num_vertices = 0
-        num_edges = 0
-        try:
-            num_vertices = int(sys.argv[1])
-            num_edges = int(sys.argv[2])
-        except:
-            print("At least one parameter not an integer!\n")
-            printUsage()
-            exit(-1)
-
-    graph, degree = createRandomGraph(num_vertices, num_edges)
-    coloring = randomizedColoring(graph, degree)
+    args = parseArguments()
+    graph, degree = createRandomGraph(args.vertices, args.edges)
+    coloring, n_iterations, num_used = randomizedColoring(graph, degree)
 
     if(testAlgorithm(graph, coloring)):
         print('Coloring succeeded!')
     else:
         print('No valid coloring found!')
+
+    printStatistics(degree, n_iterations, num_used)
+    if(args.pgraph):
+        print(f'Graph: {graph}')
+    if(args.pcoloring):
+        print(f'Coloring: {coloring}')
 
 if __name__ == "__main__":
     main()
